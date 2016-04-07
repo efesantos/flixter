47329120a327f747f60b40bd2017fe5f744f53a0
Elmer <efe_santos@hotmail.com>
2016-04-07 14:09:14 +0000

    Add enrollment requirement for users to view lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,8 @@
 class LessonsController < ApplicationController
   
+  before_action :authenticate_user!
+  before_action :require_enrolled_for_current_course
+
   def show
   end
 
@@ -8,6 +11,19 @@ class LessonsController < ApplicationController
     helper_method :current_lesson
     def current_lesson
       @current_lesson ||= Lesson.find(params[:id])
+    end
+
+    def current_course
+      #current_section = current_lesson.section
+      @current_course = current_lesson.section.course
+      #@current_course = current_section.course
+    end
+
+    def require_enrolled_for_current_course
+        unless current_user.enrolled_in?(current_course)
+          redirect_to course_path(current_course)
+          return flash[:alert] = 'Unauthorized. Enroll to view Lesson'
+        end
     end
 
 end
