a7438435c1c543eccf90a478ba9ddaa3a5dce70f
Elmer <efe_santos@hotmail.com>
2016-04-07 18:43:19 +0000

    Add ranked-model gem and configure lessons controller accordingly

--- Gemfile
+++ Gemfile
@@ -64,3 +64,5 @@ gem 'mini_magick', '~> 4.3.1'
 gem 'carrierwave-aws'
 
 gem 'stripe'
+
+gem 'ranked-model'
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,4 +3,9 @@ class Lesson < ActiveRecord::Base
   
   mount_uploader :video, VideoUploader
 
+  include RankedModel
+
+    belongs_to :section
+    ranks :row_order, :with_same => :section_id
+
 end
