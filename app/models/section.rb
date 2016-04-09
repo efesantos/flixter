35badcfc463add32143f985d7b595a025e2ff24d
Elmer <efe_santos@hotmail.com>
2016-04-09 12:49:13 +0000

    Add sortable feature to sections on instructor/course show page

--- app/models/section.rb
+++ app/models/section.rb
@@ -3,4 +3,10 @@ class Section < ActiveRecord::Base
   belongs_to :course
   has_many :lessons, dependent: :destroy
 
+  include RankedModel
+
+  belongs_to :course
+  ranks :row_order,
+    :with_same => :course_id
+
 end
