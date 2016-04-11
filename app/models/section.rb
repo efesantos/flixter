ad6351b408c1873df69c8599ac94cd262d8f8399
Elmer <efe_santos@hotmail.com>
2016-04-11 18:29:17 +0000

    Restore next_section method to originaln version

--- app/models/section.rb
+++ app/models/section.rb
@@ -10,7 +10,7 @@ class Section < ActiveRecord::Base
     :with_same => :course_id
 
   def next_section
-    section = course.sections.where("row_order < ?", self.row_order).rank(:row_order).last
+    section = course.sections.where("row_order > ?", self.row_order).rank(:row_order).first
     return section
   end
 
